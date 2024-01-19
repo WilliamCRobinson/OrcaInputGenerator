201dc51be7e7a8dd461445f8b335eeaafc028a30
prowill <william.robinson223@gmail.com>
2024-01-19 16:31:26 -0700

    added a module create_configuration_file.py which allows the user to create and specify a configuration file. Modified the main.py script to utilize this script in the absence of a config.ini file.

---
+++ create_configuration_file.py
@@ -0,0 +1,37 @@
+import os
+import configparser
+
+def create_configuration_file():
+    """
+    Create a new config.ini file based on user input for calculation parameters.
+    """
+    config = configparser.ConfigParser()
+
+    # Get user input for calculation parameters
+    config['CalculationParameters'] = {}
+    config['CalculationParameters']['method'] = input("Enter method: ")
+    config['CalculationParameters']['basis_set'] = input("Enter Basis Set: ")
+    config['CalculationParameters']['DFTFunctional'] = input("Enter DFT Functional: ")
+    config['CalculationParameters']['convergence'] = input("Enter Convergence: ")
+    config['CalculationParameters']['special'] = input("enter any special header parameters: ")
+
+    # Get xyz files in the directory called xyzdata
+    xyz_files = [file for file in os.listdir("xyzdata") if file.endswith(".xyz")]
+
+    # Create sections for each molecule in the config file
+    for i, xyz_file in enumerate(xyz_files, start=1):
+        section_name = f'Molecule{i}'
+        config[section_name] = {}
+        config[section_name]['structure_file'] = xyz_file
+        config[section_name]['charge'] = input(f"Enter Charge for {xyz_file}: ")
+        config[section_name]['spin_multiplicity'] = input(f"Enter Spin Multiplicity for {xyz_file}: ")
+
+    save_name = input("what would you like to call your configuration file: ")
+    # Save config.ini file
+    with open(save_name, 'w') as config_file:
+        config.write(config_file)
+
+    print(f"{save_name} file created successfully!")
+
+if __name__ == "__main__":
+    create_configuration_file()
--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def main():
     Main function orchestrating the generation of Orca input files.
 
     Workflow:
-    1. Initialize ConfigurationReader to read molecular configurations and calculation parameters from config.ini.
+    1. Initialize ConfigurationReader to read molecular configurations and calculation parameters from a config.ini
+       file.
     2. Load molecular configurations and calculation parameters.
     3. Initialize InputGenerator and load the template Orca input file.
     4. Iterate over molecular configurations, generate Orca input files, and save them to the output directory.
@@ -22,9 +23,15 @@ def main():
     print("****")
     print("Initializing config reader...")
     configuration_reader = ConfigurationReader()
+    # while running this it should call the create_configuration_file if there is no config.ini
+    if not os.path.exists("config.ini"):
+        print("config.ini not found. Creating a new one...")
+        os.system("python create_configuration_file.py")
+
+    config_to_use = input("please enter the exact name of your .ini file: ")
 
     # Load config file
-    if configuration_reader.load_config_file("config.ini"):
+    if configuration_reader.load_config_file(config_to_use):
         print("****")
         print("Reading calculation and molecular parameters...")
 
@@ -54,7 +61,10 @@ def main():
                 print("Input content exists! Saving!!")
                 output_filename = f'{config_name}.inp'
                 input_generator.save_input_file(output_filename, input_content)
-                os.rename(output_filename, f"output\\{output_filename}")
+                try:
+                    os.rename(output_filename, f"output\\{output_filename}")
+                except Exception as e:
+                    print(f"Error, could not move output file:{e}")
             else:
                 print("No input content to add! Woe is you.\n")
 
